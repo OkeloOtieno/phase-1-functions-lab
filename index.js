f65d2c8fda325fce5adea05d9da05f4259e5d9f0
Emmanuel <emmanuelokello294@gmail.com>
2024-03-18 22:50:22 +0300

    completes functions lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+
+const BLOCK_LENGTH_IN_FEET = 264;
+
+function distanceFromHqInBlocks(blockNumber) {
+ return Math.abs(blockNumber - 42);
+}
+
+function distanceFromHqInFeet(blockNumber) {
+ return distanceFromHqInBlocks(blockNumber) * BLOCK_LENGTH_IN_FEET;
+}
+
+function distanceTravelledInFeet(start, destination) {
+ return Math.abs(distanceFromHqInFeet(start) - distanceFromHqInFeet(destination));
+}
+
+function calculatesFarePrice(start, destination) {
+ const totalFeetTravelled = distanceTravelledInFeet(start, destination);
+ if (totalFeetTravelled > 2500) {
+    return 'cannot travel that far';
+ }
+ if (totalFeetTravelled <= 400) {
+    return 0;
+ }
+ const fare = (totalFeetTravelled - 400) * 0.02;
+ return fare > 25 ? 25 : fare;
+}
+
